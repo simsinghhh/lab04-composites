6130a7b069701079007b35a7f2890919df7740bf
simsinghhh <hisimrat@gmail.com>
2017-01-25 11:24:56 -0800

    uploading lab

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,22 @@ interface IDomElement {
   print();
 }
 
+class TextNode implements IDomElement {
+  private text: string;
+  constructur(text:string) {
+    this.text = text;
+  }
+
+  print() {
+    console.log(this.text);
+  }
+}
+
+class DomElement implements IDomElement {
+  elementName: string;
+  textContent: string;
+  elements: IDomElementp[];
+}
+
+
+
